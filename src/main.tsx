fc8c3a62386c7501ff8eaa54d0c4416e19c3b21e
Rahma Maulida <rahmaulidaaaa@gmail.com>
2026-07-16 02:03:22 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
   createRoot(document.getElementById("root")!).render(<App />);
   
